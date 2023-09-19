dbb31fa05adefccd71e45f1d00e855e4ed2639f5
Stas_Che <86572188+Chetverukhin@users.noreply.github.com>
2023-09-19 21:54:11 +0400

    change models

--- Test/Shared/Genre.cs
+++ Test/Shared/Genre.cs
@@ -5,5 +5,10 @@
         public string GenreId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public List<Product> Products { get; set; }
+        public Genre()
+        {
+            Products = new List<Product>();
+        }
     }
 }
--- Test/Shared/Product.cs
+++ Test/Shared/Product.cs
@@ -5,7 +5,7 @@
         public string ProductId { get; set; }
         public string Name { get; set; }
         public decimal Cost { get; set; }
-        public string Genre { get; set; }
         public string Author { get; set; }
+        public Genre Genre { get; set;}
     }
 }
