e3ce7b2fb5aec694aa9bc63c3ae7d4ff0bc32644
Stas_Che <86572188+Chetverukhin@users.noreply.github.com>
2023-09-19 23:06:47 +0400

    add product controllers

---
+++ Test/Server/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test.Server.Data;
+
+namespace Test.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ProjectDdContext _context;
+
+        public ProductController(ProjectDdContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
